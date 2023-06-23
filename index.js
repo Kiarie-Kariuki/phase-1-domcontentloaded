c23e777b7bc7807209563dd562fc8e3bffbb4f00
Collins <collinskiarie13@outlook.com>
2023-06-23 14:41:12 +0300

    Completed task

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+function updateTextContent() {
+    const paragraph = document.querySelector('p');
+    paragraph.textContent = "This is really cool!";
+  }
+  
+  document.addEventListener('DOMContentLoaded', updateTextContent);
+  
